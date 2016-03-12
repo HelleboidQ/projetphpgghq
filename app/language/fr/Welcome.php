61342c59e143d68f8e763071c6e25391d902f770
Gilles <gilux762@gmail.com>
2016-03-12 11:35:51 +0100

    Modif welcome

--- app/language/fr/Welcome.php
+++ app/language/fr/Welcome.php
@@ -5,7 +5,7 @@
 return [
 
     // Index method
-    'welcome_text'    => 'Bienvenue',
+    'welcome_text'    => 'Bienvenue !',
     'welcome_message' => '
 		Bonjour, bienvenue dans le contrôleur de bienvenue ! <br/>
 		Ce contenu peut être changé via le fichier: <code>app/views/welcome/welcome.php</code>
