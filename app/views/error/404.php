c669bb7062729bb1905397ec19b1bd18d586a885
Gilux <gilles@ewill.fr>
2016-04-04 22:22:02 +0200

    Teasing sur la 404
    Ça rigole plus.

--- app/views/error/404.php
+++ app/views/error/404.php
@@ -11,18 +11,11 @@ use Core\Error;
 
 			<h1>404</h1>
 
-			<?php echo $data['error']; ?>
-
 			<hr />
 
-			<h3>The page you were looking for could not be found.</h3>
-			<p>This could be the result of the page being removed, the name being changed or the page being temporarily unavailable</p>
-			<h3>Troubleshooting</h3>
-
-			<ul>
-			  <li>If you spelled the URL manually, double check the spelling</li>
-			  <li>Go to our website's home page, and navigate to the content in question</li>
-			</ul>
+			<h3>On a fait tout ce qu'on pouvait, mais ta page n'est pas ici.</h3>
+			<p>Bientôt et puisqu'on pense à toi, un jeu de casse-briques débordant de swag sera bientôt disponible ici.</p>
+			
 
 		</div>
 	</div>
